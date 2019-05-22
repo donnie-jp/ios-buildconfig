e0351a8d188ea1396b0775c0a0ca090227c43b8f
Smith, Donnie <donnie.smith@rakuten.com>
2019-05-22 19:26:26 +0900

    docs: add fastlane action to upload docs to ftp SDKCF-953

--- fastlane/actions/upload_docs.rb
+++ fastlane/actions/upload_docs.rb
@@ -8,11 +8,11 @@ module Fastlane
           begin
             size = ftp.size(item)
             if size.is_a? Numeric
-                UI.message("File #{item} size is #{size}")
+                #UI.message("File #{item} size is #{size}")
                 return true
           end
           rescue Net::FTPPermError
-            UI.message("Item #{item} is a directory")
+            #UI.message("Item #{item} is a directory")
             return false
           end
       end
@@ -20,10 +20,10 @@ module Fastlane
       # remove all files recursively at ftp path
       def self.rm_r (ftp, path)
         if remote_item_is_file?(ftp, path)
-          UI.message "Dry run - delete file #{path}"
-          #ftp.delete(path)
+          UI.message "Delete file #{path}"
+          ftp.delete(path)
         else
-          UI.message "Change FTP folder to #{path}"
+          #UI.message "Change FTP folder to #{path}"
           ftp.chdir(path)
 
           begin
@@ -34,66 +34,66 @@ module Fastlane
           rescue Net::FTPTempError
             # maybe all files were deleted already
           end
-          UI.message "Dry run - delete folder #{path}"
-          #ftp.rmdir(path)
+          UI.message "Delete folder #{path}"
+          ftp.rmdir(path)
         end
       end
 
       def self.run(params)
-
-      UI.message("Current local dir " + Dir.pwd)
-      if Helper.ci?
-        Dir.chdir(ENV['WORKSPACE'])
-      end
-      Dir.chdir('artifacts')
-
-      host = params[:host] || ENV['FTP_REM_HOST']
-      folder = params[:upload_folder]
-      user = params[:user] || ENV['FTP_REM_USER']
-      password = params[:password] || ENV['FTP_REM_PASSWORD']
-
-      ftp = DoubleBagFTPS.new
-      ftp.ssl_context = DoubleBagFTPS.create_ssl_context(:verify_mode => OpenSSL::SSL::VERIFY_NONE)
-
-      ftp.connect(host)
-      UI.message("Log in to #{host}, with user #{user}")
-      ftp.login(user, password)
-      ftp.passive = true
-      UI.success("Successful login to #{host}. Now attempt to upload #{folder}.")
-
-      ftproot = '/site/wwwroot/ios-sdk'
-      if params[:testing]
-        ftproot = ftproot + '/_test'
-      end
-      ftp.chdir(ftproot)
-      UI.message 'Changed remote dir to ' + ftp.pwd
-
-      localFiles = Dir.glob("#{folder}/**/*").sort
-      # print entries
-
-      begin
-        ftp.mkdir(folder)
-      rescue
-        # TODO: folder exists - need to empty contents and delete folder
-        # before uploading files
-        UI.message "Remote folder #{folder} exists. Need to empty it first"
-        rm_r(ftp, "#{ftproot}/#{folder}")
-      end
-
-      localFiles.each do |name|
-        if File::directory? name
-          UI.message 'Create remote dir ' + name
-          ftp.mkdir(name)
-        else
-          File.open(name) { |file|
-            #ftp.storbinary("STOR #{name}", file, 1024)
-            ftp.putbinaryfile(file, name)
-          }
+        UI.message("Current local dir " + Dir.pwd)
+        if Helper.ci?
+          Dir.chdir(ENV['WORKSPACE'])
         end
-      end
-      UI.success("Upload complete: folder listing\n")
-      puts ftp.nlst
-      ftp.close()
+        Dir.chdir('artifacts')
+
+        host = params[:host] || ENV['FTP_REM_HOST']
+        folder = params[:upload_folder]
+        user = params[:user] || ENV['FTP_REM_USER']
+        password = params[:password] || ENV['FTP_REM_PASSWORD']
+
+        ftp = DoubleBagFTPS.new
+        ftp.ssl_context = DoubleBagFTPS.create_ssl_context(:verify_mode => OpenSSL::SSL::VERIFY_NONE)
+
+        ftp.connect(host)
+        UI.message("Log in to #{host}, with user #{user}")
+        ftp.login(user, password)
+        ftp.passive = true
+        UI.success("Successful login to #{host}. Now attempt to upload #{folder}.")
+
+        ftproot = '/site/wwwroot/ios-sdk'
+        if params[:testing]
+          ftproot = ftproot + '/_test'
+        end
+        ftp.chdir(ftproot)
+        UI.message 'Changed remote dir to ' + ftp.pwd
+
+        localFiles = Dir.glob("#{folder}/**/*").sort
+        # print entries
+
+        begin
+          ftp.mkdir(folder)
+        rescue
+          # TODO: folder exists - need to empty contents and delete folder
+          # before uploading files
+          UI.message "Remote folder #{folder} already exists so deleting it now."
+          rm_r(ftp, "#{ftproot}/#{folder}")
+          ftp.mkdir(folder)
+        end
+
+        localFiles.each do |name|
+          if File::directory? name
+            UI.message 'Create remote dir ' + name
+            ftp.mkdir(name)
+          else
+            File.open(name) { |file|
+              #ftp.storbinary("STOR #{name}", file, 1024)
+              UI.message 'Put file ' + name
+              ftp.putbinaryfile(file, name)
+            }
+          end
+        end
+        UI.success("Documentation upload complete")
+        ftp.close()
       end
 
       def self.description
