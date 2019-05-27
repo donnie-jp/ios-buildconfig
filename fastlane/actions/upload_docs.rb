d357a90f02db198984c88a241daac9597938e603
Smith, Donnie <donnie.smith@rakuten.com>
2019-05-27 18:21:32 +0900

    docs: add fastlane action to upload docs to ftp SDKCF-953

--- fastlane/actions/upload_docs.rb
+++ fastlane/actions/upload_docs.rb
@@ -4,15 +4,14 @@ require 'double_bag_ftps'
 module Fastlane
   module Actions
     class UploadDocsAction < Action
+
       def self.remote_item_is_file? (ftp, item)
           begin
             size = ftp.size(item)
             if size.is_a? Numeric
-                #UI.message("File #{item} size is #{size}")
                 return true
           end
           rescue Net::FTPPermError
-            #UI.message("Item #{item} is a directory")
             return false
           end
       end
@@ -20,10 +19,8 @@ module Fastlane
       # remove all files recursively at ftp path
       def self.rm_r (ftp, path)
         if remote_item_is_file?(ftp, path)
-          #UI.message "Delete file #{path}"
           ftp.delete(path)
         else
-          #UI.message "Change FTP folder to #{path}"
           ftp.chdir(path)
 
           begin
@@ -44,51 +41,47 @@ module Fastlane
         if Helper.ci?
           Dir.chdir(ENV['WORKSPACE'])
         end
-        Dir.chdir('artifacts')
 
         host = params[:host] || ENV['FTP_REM_HOST']
-        folder = params[:upload_folder]
+        folder = params[:upload_from]
+        Dir.chdir(folder)
         user = params[:user] || ENV['FTP_REM_USER']
         password = params[:password] || ENV['FTP_REM_PASSWORD']
 
+        # Microsoft host only allows FTPS
         ftp = DoubleBagFTPS.new
         ftp.ssl_context = DoubleBagFTPS.create_ssl_context(:verify_mode => OpenSSL::SSL::VERIFY_NONE)
 
         ftp.connect(host)
-        UI.message("Log in to #{host}, with user #{user}")
         ftp.login(user, password)
         ftp.passive = true
-        UI.success("Successful login to #{host}. Now attempt to upload #{folder}.")
+        UI.success("Successful login to #{host}. Now attempt to upload contents of #{folder}.")
 
         ftproot = '/site/wwwroot/ios-sdk'
         if params[:testing]
           ftproot = ftproot + '/_test'
         end
+
         ftp.chdir(ftproot)
         UI.message 'Changed remote dir to ' + ftp.pwd
 
-        localFiles = Dir.glob("#{folder}/**/*").sort
-        # print entries
-
-        begin
-          ftp.mkdir(folder)
-        rescue
-          # TODO: folder exists - need to empty contents and delete folder
-          # before uploading files
-          UI.message "Remote folder #{folder} already exists so deleting it now..."
-          rm_r(ftp, "#{ftproot}/#{folder}")
-          UI.message "After deleting #{ftproot}/#{folder} the remote dir is #{ftp.pwd}"
-          ftp.chdir(ftproot)
-          ftp.mkdir(folder)
-        end
+        localFiles = Dir.glob("**/*").sort
+        UI.message "Folder listing for #{folder}: \n#{Dir.glob("*")}"
 
         localFiles.each do |name|
           if File::directory? name
             UI.message 'Create remote dir ' + name
-            ftp.mkdir(name)
+            begin
+              ftp.mkdir(name)
+            rescue
+              UI.message "Remote folder #{name} already exists so deleting it now..."
+              rm_r(ftp, "#{ftproot}/#{name}")
+              UI.message "After deleting #{ftproot}/#{name} the remote dir is #{ftp.pwd}"
+              ftp.chdir(ftproot)
+              ftp.mkdir(name)
+            end
           else
             File.open(name) { |file|
-              #ftp.storbinary("STOR #{name}", file, 1024)
               #UI.message 'Put file ' + name
               ftp.putbinaryfile(file, name)
             }
@@ -104,22 +97,28 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :upload_folder,
-                                       description: "Folder to upload"
+          FastlaneCore::ConfigItem.new(key: :upload_from,
+                                       description: "Folder to upload",
+                                       default_value: "artifacts/docs",
+                                       optional: true
                                       ),
           FastlaneCore::ConfigItem.new(key: :host,
-                                       description: "FTP host"
+                                       description: "FTP host",
+                                       optional: true
                                       ),
           FastlaneCore::ConfigItem.new(key: :user,
-                                       description: "Username"
+                                       description: "Username",
+                                       optional: true
                                       ),
           FastlaneCore::ConfigItem.new(key: :password,
-                                       description: "Password"
+                                       description: "Password",
+                                       optional: true
                                       ),
           FastlaneCore::ConfigItem.new(key: :testing,
                                        description: "Set true if testing (will upload to ios-sdk/_test)",
                                        default_value: false,
-                                       is_string: false
+                                       is_string: false,
+                                       optional: true
                                        )
         ]
       end
